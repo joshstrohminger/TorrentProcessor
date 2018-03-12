2dca0863fe7480e2ccff9c71aaa108b3475d1edc
Josh Strohminger <jdst8-git@yahoo.com>
2018-03-11 20:04:17 -0700

    update required args

--- app.js
+++ app.js
@@ -3,22 +3,22 @@
 const argv = require('yargs')
     .command('copy', 'Copy completed files', (yargs) => {
         return yargs
-            .option('N', { description: 'Torrent name'})
-            .option('L', { description: 'Category'})
-            .option('F', { description: 'Content path (same as root path for multifile torrent)'})
-            .option('R', { description: 'Root path (first torrent subdirectory path)'})
-            .option('D', { description: 'Save path'})
-            .option('C', { description: 'Number of files'})
-            .option('Z', { description: 'Torrent size (bytes)'})
-            .option('T', { description: 'Current tracker'})
-            .option('I', { description: 'Info hash'})
-            .demandOption(['N','L','F','R','D','C','Z','T','I']);
-    }, copy)
+            .option('n', { alias: 'N', demandOption: true, requiresArg: true, type: 'string', description: 'Torrent name'})
+            .option('l', { alias: 'L', demandOption: true, requiresArg: true, type: 'string', description: 'Category', choices: ['Moviesingle','TvSingle']})
+            .option('f', { alias: 'F', demandOption: true, requiresArg: true, type: 'string', description: 'Content path (same as root path for multifile torrent)'})
+            .option('r', { alias: 'R', demandOption: true, requiresArg: true, type: 'string', description: 'Root path (first torrent subdirectory path)'})
+            .option('d', { alias: 'D', demandOption: true, requiresArg: true, type: 'string', description: 'Save path'})
+            .option('c', { alias: 'C', demandOption: true, requiresArg: true, type: 'string', description: 'Number of files'})
+            .option('z', { alias: 'Z', demandOption: true, requiresArg: true, type: 'string', description: 'Torrent size (bytes)'})
+            .option('t', { alias: 'T', demandOption: true, requiresArg: true, type: 'string', description: 'Current tracker'})
+            .option('i', { alias: 'I', demandOption: true, requiresArg: true, type: 'string', description: 'Info hash'});
+    }, copyCommandHandler)
     .demandCommand()
     .help('h')
+    .alias('h', 'H')
     .alias('h', 'help')
     .argv;
 
-function copy(argv) {
+function copyCommandHandler(argv) {
     console.log('Copying files...');
 }
